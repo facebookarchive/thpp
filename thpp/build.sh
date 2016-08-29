953baf6f0ab1011642e105b9ca4a6db9d565d2f3
Zeming Lin <misterabc@fb.com>
2016-08-29 11:50:16 -0700

    Updating build file after gtest link is invalidated
    Summary: changed gtest link to a github release instead
    
    Reviewed By: soumith
    
    Differential Revision: D3786121
    
    fbshipit-source-id: b45077538602cd3bc12deef524b7358bc2026002

--- thpp/build.sh
+++ thpp/build.sh
@@ -27,14 +27,14 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         SHA="shasum"
 fi
 
-rm -rf gtest-1.7.0 gtest-1.7.0.zip
-curl -JLOk https://googletest.googlecode.com/files/gtest-1.7.0.zip
-if [[ $($SHA -b gtest-1.7.0.zip | cut -d' ' -f1) != \
-      'f85f6d2481e2c6c4a18539e391aa4ea8ab0394af' ]]; then
-  echo "Invalid gtest-1.7.0.zip file" >&2
+rm -rf googletest-release-1.7.0 googletest-release-1.7.0.zip
+curl -JLOk https://github.com/google/googletest/archive/release-1.7.0.zip
+if [[ $($SHA -b googletest-release-1.7.0.zip | cut -d' ' -f1) != \
+      'f89bc9f55477df2fde082481e2d709bfafdb057b' ]]; then
+  echo "Invalid googletest-release-1.7.0.zip file" >&2
   exit 1
 fi
-unzip gtest-1.7.0.zip
+unzip googletest-release-1.7.0.zip
 
 # Build in a separate directory
 mkdir -p build
